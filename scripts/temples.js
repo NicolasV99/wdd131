907a0b5c284569151e9741fbfb01a302c43574c8
NicolasV99 <legolasnicolas99@gmail.com>
2024-06-21 21:39:06 -0500

    create temples files, HTML structure and some CSS code

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+// Update the current year
+const currentYearElement = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = currentYear;
+
+// Update the last modified date
+const lastModifiedElement = document.getElementById('lastModified');
+const lastModifiedDate = new Date(document.lastModified);
+lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
